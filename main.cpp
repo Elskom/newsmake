e7c8a858dc4b95564523889c64932a7a6985bfdc
Sean D. Hunt <seandhunt_7@yahoo.com>
2018-01-16 13:31:11 -0500

    Added entry point for platform independent tool.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <experimental/filesystem>
+
+int main(int argc, char **argv)
+{
+  // get the current directory the invoked this program.
+  // look for an *.master file.
+  // process the *.master file settings.
+  // process the *.master file’s imports.
+  // write data to the output file set in the *.master file.
+  return 0;
+}
