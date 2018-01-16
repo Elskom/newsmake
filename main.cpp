7dc1a32c40a2d48410aa99ba990f2518d228fc8f
AraHaan <seandhunt_7@yahoo.com>
2018-01-16 18:00:53 -0500

    Skip comments in master files.

--- main.cpp
+++ main.cpp
@@ -10,25 +10,31 @@ int main(int argc, char *argv[])
   std::string ext(".master");
   for(auto& p: fs::recursive_directory_iterator(fs::current_path())
   {
-    // look for an *.master file.
     if(p.path().extension() == ext())
     {
-      // send processing message to user.
       std::cout << "Processing " << p.filename() << "..." << std::endl;
-      // open *.master file.
       std::ifstream master_file(p);
       for (std::string line; std::getline(master_file, line);)
       {
-        // process the *.master file settings.
-        
-        // process the *.master file’s imports.
-        
-        // open the imported files in the folder name of the import.
-        // get imported files data.
-        // close imported files.
-        // write data to the output file set in the *.master file.
+        if (line.find("# ") == 0)
+        {
+          // skip the comments in the master file.
+        }
+        else
+        {
+          // process the *.master file’s settings.
+          
+          // process the *.master file’s imports.
+          
+          // open the imported files in the folder name of the import.
+          
+          // get imported files data.
+          
+          // close imported files.
+          
+          // write data to the output file set in the *.master file.
+        }
       }
-      // close *.master file.
       master_file.close();
     }
   }
