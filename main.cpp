8e542fbc7f301ecbe4a375d89345ae7a69fd7e4b
AraHaan <seandhunt_7@yahoo.com>
2018-01-16 20:14:57 -0500

    Start on the line processing.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ namespace fs = std::experimental::filesystem;
 int main(int argc, char *argv[])
 {
   std::string ext(".master");
+  std::string project_name;
+  std::string outputfile_name;
   for(auto& p: fs::recursive_directory_iterator(fs::current_path())
   {
     if(p.path().extension() == ext())
@@ -22,8 +24,29 @@ int main(int argc, char *argv[])
         }
         else
         {
+          //projname = "Els_kom"
+          //genfilename = "../../bin/x86/Release/news.txt"
+          //tabs = true
+          //deletechunkentryfiles = false
           // process the *.master file’s settings.
-          
+          if (line.find("projname = \"") == 0)
+          {
+            // project name set.
+          }
+          else
+          {
+            std::cout << "Error: Project name not set." << std::endl;
+            return 1;
+          }
+          if (line.find("genfilename = \"") == 0)
+          {
+            // generated output file name set.
+          }
+          else
+          {
+            std::cout << "Error: generated output file name not set." << std::endl;
+            return 1;
+          }
           // process the *.master file’s imports.
           
           // open the imported files in the folder name of the import.
