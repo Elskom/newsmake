cabc0a82ffc07ef77ca281279ffdd3926b86ffc0
Sean D. Hunt <seandhunt_7@yahoo.com>
2018-11-15 15:24:35 -0500

    Set up cpack and add a version checking command.

--- main.cpp
+++ main.cpp
@@ -76,161 +76,181 @@ void formatline(std::string &input, bool tabs, bool output_format_md,
 }
 
 int main(int argc, char *argv[]) {
-  std::string ext = ".master";
-  std::string project_name;
-  std::string outputfile_name;
-  bool tabs = true;
-  bool devmode = false;
-  bool delete_files = true;
-  bool first_import = true;
-  bool found_master_file = false;
-  bool output_format_md = false;
-  std::vector<std::string> section_data;
-  for (auto &p : fs::recursive_directory_iterator(fs::current_path())) {
-    if (p.path().extension().string() == ext) {
-      found_master_file = true;
-      std::cout << "Processing " << p.path().filename().string() << "..."
-                << std::endl;
-      std::ifstream master_file(p.path().string());
-      for (std::string line; std::getline(master_file, line);) {
-        if (line.find("# ") != 0) {
-          if (line.find("projname = \"") == 0) {
-            project_name = line;
-            project_name.erase(0, 12);
-            project_name.erase(project_name.length() - 1, 1);
-          } else if (line.find("projname = \"") != 0 && project_name.empty()) {
-            std::cout << "Error: Project name not set." << std::endl;
-            return 1;
-          } else if (line.find("devmode = ") == 0) {
-            devmode = line.compare("devmode = true") == 0;
-          } else if (line.find("genfilename = \"") == 0) {
-            outputfile_name = line;
-            outputfile_name.erase(0, 15);
-            outputfile_name.erase(outputfile_name.length() - 1, 1);
-          } else if (line.find("genfilename = \"") != 0 &&
-                     outputfile_name.empty()) {
-            std::cout << "Error: generated output file name not set."
-                      << std::endl;
-            return 1;
-          } else if (line.find("tabs = ") == 0) {
-            tabs = line.compare("tabs = true") == 0;
-          } else if (line.find("deletechunkentryfiles = ") == 0) {
-            if (!devmode) {
-              delete_files = line.compare("deletechunkentryfiles = true") == 0;
-            } else {
-              delete_files = false;
-            }
-          }
-          // user can now output as markdown.
-          else if (line.find("outputasmd = ") == 0) {
-            output_format_md = line.compare("outputasmd = true") == 0;
-          } else if (line.find("import \"") == 0) {
-            std::string imported_folder = line;
-            imported_folder.erase(0, 8);
-            imported_folder.erase(imported_folder.length() - 1, 1);
-            std::string section_string;
-            if (first_import) {
-              if (output_format_md) {
-                section_string = "Whats new in v";
-              } else {
-                section_string = "                          Whats new in v";
-              }
-              section_string += imported_folder;
-              section_string += "\n============================================"
-                                "==================================\n";
-              first_import = false;
-            } else {
-              if (!output_format_md) {
-                // if the section divider is not here the resulting markdown
-                // would look like trash.
-                section_string =
-                    "\n============================================="
-                    "=================================\n";
-                section_string += "                             ";
-              }
-              section_string += project_name;
-              section_string += " v";
-              section_string += imported_folder;
-              section_string += "\n============================================"
-                                "==================================\n";
-            }
-            std::string section_text;
-            if (fs::exists(fs::path(fs::current_path().string() + '/' +
-                                    imported_folder))) {
-              for (auto &imported_path :
-                   fs::recursive_directory_iterator(fs::path(
-                       fs::current_path().string() + '/' + imported_folder))) {
-                std::ifstream entry_item(imported_path.path().string());
-                std::string temp;
-                if (!output_format_md) {
-                  if (tabs) {
-                    temp = "\t+ ";
-                  } else {
-                    temp = "    + ";
-                  }
-                } else {
-                  temp = "+ ";
-                }
-                for (std::string entry_line;
-                     std::getline(entry_item, entry_line);) {
-                  temp += entry_line;
-                }
-                formatline(temp, tabs, output_format_md);
-                temp += '\n';
-                section_text += temp;
-                entry_item.close();
-              }
-            }
-            if (delete_files && !section_text.empty()) {
-              // save section text and then delete the folder.
-              std::ofstream section_file(fs::current_path().string() + '/' +
-                                         imported_folder + ".section");
-              section_file << section_text;
-              section_file.close();
-              for (auto &imported_path :
-                   fs::recursive_directory_iterator(fs::path(
-                       fs::current_path().string() + '/' + imported_folder))) {
-                fs::remove(imported_path);
-              }
-              fs::remove(fs::path(fs::current_path().string() + '/' +
-                                  imported_folder));
-            }
-            if (section_text.empty()) {
-              // load saved *.section file.
-              if (fs::exists(fs::path(fs::current_path().string() + '/' +
-                                      imported_folder + ".section"))) {
-                std::ifstream section_file(fs::current_path().string() + '/' +
-                                           imported_folder + ".section");
-                for (std::string entry_line;
-                     std::getline(section_file, entry_line);) {
-                  section_text += entry_line + '\n';
-                }
-                section_file.close();
-              }
-            }
-            section_string += section_text;
-            section_data.push_back(section_string);
-          }
-        }
-      }
-      if (!outputfile_name.empty()) {
-        if (!fs::exists(fs::path(outputfile_name))) {
-          fs::create_directories(fs::path(outputfile_name).parent_path());
-        }
-        std::ofstream output_file(outputfile_name);
-        for (const auto &section : section_data) {
-          output_file << section;
-        }
-        output_file.close();
-        std::cout << "Successfully Generated '" << outputfile_name << "'."
-                  << std::endl;
-      }
-      master_file.close();
+  if (argc > 1) {
+    std::string command = argv[1];
+    if (command.compare("--version") == 0) {
+      std::cout << "Version: 1.0.0" << std::endl;
     }
   }
-  if (!found_master_file) {
-    std::cout << "Error: no *.master file found." << std::endl;
-    return 1;
+  else {
+    std::string ext = ".master";
+    std::string project_name;
+    std::string outputfile_name;
+    bool tabs = true;
+    bool devmode = false;
+    bool delete_files = true;
+    bool first_import = true;
+    bool found_master_file = false;
+    bool output_format_md = false;
+    std::vector<std::string> section_data;
+    for (auto &p : fs::recursive_directory_iterator(fs::current_path())) {
+      if (p.path().extension().string() == ext) {
+        found_master_file = true;
+        std::cout << "Processing " << p.path().filename().string() << "..."
+          << std::endl;
+        std::ifstream master_file(p.path().string());
+        for (std::string line; std::getline(master_file, line);) {
+          if (line.find("# ") != 0) {
+            if (line.find("projname = \"") == 0) {
+              project_name = line;
+              project_name.erase(0, 12);
+              project_name.erase(project_name.length() - 1, 1);
+            }
+            else if (line.find("projname = \"") != 0 && project_name.empty()) {
+              std::cout << "Error: Project name not set." << std::endl;
+              return 1;
+            }
+            else if (line.find("devmode = ") == 0) {
+              devmode = line.compare("devmode = true") == 0;
+            }
+            else if (line.find("genfilename = \"") == 0) {
+              outputfile_name = line;
+              outputfile_name.erase(0, 15);
+              outputfile_name.erase(outputfile_name.length() - 1, 1);
+            }
+            else if (line.find("genfilename = \"") != 0 &&
+              outputfile_name.empty()) {
+              std::cout << "Error: generated output file name not set."
+                << std::endl;
+              return 1;
+            }
+            else if (line.find("tabs = ") == 0) {
+              tabs = line.compare("tabs = true") == 0;
+            }
+            else if (line.find("deletechunkentryfiles = ") == 0) {
+              if (!devmode) {
+                delete_files = line.compare("deletechunkentryfiles = true") == 0;
+              }
+              else {
+                delete_files = false;
+              }
+            }
+            // user can now output as markdown.
+            else if (line.find("outputasmd = ") == 0) {
+              output_format_md = line.compare("outputasmd = true") == 0;
+            }
+            else if (line.find("import \"") == 0) {
+              std::string imported_folder = line;
+              imported_folder.erase(0, 8);
+              imported_folder.erase(imported_folder.length() - 1, 1);
+              std::string section_string;
+              if (first_import) {
+                if (output_format_md) {
+                  section_string = "Whats new in v";
+                }
+                else {
+                  section_string = "                          Whats new in v";
+                }
+                section_string += imported_folder;
+                section_string += "\n============================================"
+                  "==================================\n";
+                first_import = false;
+              }
+              else {
+                if (!output_format_md) {
+                  // if the section divider is not here the resulting markdown
+                  // would look like trash.
+                  section_string =
+                    "\n============================================="
+                    "=================================\n";
+                  section_string += "                             ";
+                }
+                section_string += project_name;
+                section_string += " v";
+                section_string += imported_folder;
+                section_string += "\n============================================"
+                  "==================================\n";
+              }
+              std::string section_text;
+              if (fs::exists(fs::path(fs::current_path().string() + '/' +
+                imported_folder))) {
+                for (auto &imported_path :
+                  fs::recursive_directory_iterator(fs::path(
+                    fs::current_path().string() + '/' + imported_folder))) {
+                  std::ifstream entry_item(imported_path.path().string());
+                  std::string temp;
+                  if (!output_format_md) {
+                    if (tabs) {
+                      temp = "\t+ ";
+                    }
+                    else {
+                      temp = "    + ";
+                    }
+                  }
+                  else {
+                    temp = "+ ";
+                  }
+                  for (std::string entry_line;
+                    std::getline(entry_item, entry_line);) {
+                    temp += entry_line;
+                  }
+                  formatline(temp, tabs, output_format_md);
+                  temp += '\n';
+                  section_text += temp;
+                  entry_item.close();
+                }
+              }
+              if (delete_files && !section_text.empty()) {
+                // save section text and then delete the folder.
+                std::ofstream section_file(fs::current_path().string() + '/' +
+                  imported_folder + ".section");
+                section_file << section_text;
+                section_file.close();
+                for (auto &imported_path :
+                  fs::recursive_directory_iterator(fs::path(
+                    fs::current_path().string() + '/' + imported_folder))) {
+                  fs::remove(imported_path);
+                }
+                fs::remove(fs::path(fs::current_path().string() + '/' +
+                  imported_folder));
+              }
+              if (section_text.empty()) {
+                // load saved *.section file.
+                if (fs::exists(fs::path(fs::current_path().string() + '/' +
+                  imported_folder + ".section"))) {
+                  std::ifstream section_file(fs::current_path().string() + '/' +
+                    imported_folder + ".section");
+                  for (std::string entry_line;
+                    std::getline(section_file, entry_line);) {
+                    section_text += entry_line + '\n';
+                  }
+                  section_file.close();
+                }
+              }
+              section_string += section_text;
+              section_data.push_back(section_string);
+            }
+          }
+        }
+        if (!outputfile_name.empty()) {
+          if (!fs::exists(fs::path(outputfile_name))) {
+            fs::create_directories(fs::path(outputfile_name).parent_path());
+          }
+          std::ofstream output_file(outputfile_name);
+          for (const auto &section : section_data) {
+            output_file << section;
+          }
+          output_file.close();
+          std::cout << "Successfully Generated '" << outputfile_name << "'."
+            << std::endl;
+        }
+        master_file.close();
+      }
+    }
+    if (!found_master_file) {
+      std::cout << "Error: no *.master file found." << std::endl;
+      return 1;
+    }
   }
   return 0;
 }
