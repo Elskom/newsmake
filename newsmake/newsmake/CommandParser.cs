cc634ed075f9f460d933f6d8da646cc7f797cf19
Fisu Nakato <seandhunt_7@yahoo.com>
2019-05-04 14:42:00 -0400

    Changes to things.

---
+++ newsmake/newsmake/CommandParser.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) 2018-2019, Els_kom org.
+// https://github.com/Elskom/
+// All rights reserved.
+// license: GPL, see LICENSE for more details.
+
+namespace Newsmake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    internal class CommandParser
+    {
+        private readonly string[] args;
+        private readonly List<Command> cmds;
+
+        public CommandParser(string[] args)
+        {
+            this.args = args;
+            this.cmds = new List<Command>();
+        }
+
+        public int Length
+            => this.args.Length;
+
+        public bool HasDocs { get; set; } = false;
+
+        public string DocsUrl { get; set; } = string.Empty;
+
+        public static CommandParser operator +(CommandParser parsor, Command cmd)
+        {
+            parsor.AddCommand(cmd);
+            return parsor;
+        }
+
+        public static CommandParser operator +(CommandParser parsor, Command[] cmds)
+        {
+            parsor.AddCommands(cmds);
+            return parsor;
+        }
+
+        public void AddCommand(Command cmd)
+            => this.cmds.Add(cmd);
+
+        public void AddCommands(Command[] cmds)
+            => this.cmds.AddRange(cmds);
+
+        public void ProcessCommands()
+        {
+            var cmdgroup = string.Empty;
+            var foundcmd = false;
+            var foundgrp = false;
+            var currentArg = string.Empty;
+            foreach (var arg in this.args)
+            {
+                currentArg = arg;
+                foreach (var cmd in this.cmds)
+                {
+                    if (cmd.CommandGroup.Equals(arg))
+                    {
+                        foundgrp = true;
+                        cmdgroup = cmd.CommandGroup;
+                    }
+
+                    if (cmd.Equals(arg) && (cmd.CommandGroup.Equals(cmdgroup) || cmd.CommandGroup.Equals("Global")))
+                    {
+                        foundcmd = true;
+
+                        // now we got to filter the commands, stripping the ones already processed for passing to the invoked command.
+                        var tmp = this.args.ToList();
+                        var index = tmp.IndexOf(arg);
+                        tmp.RemoveRange(0, index + 1);
+                        cmd.InvokeCommand(tmp.ToArray());
+                        tmp.Clear();
+                    }
+                }
+            }
+
+            if (!foundcmd && !foundgrp)
+            {
+                Console.Error.WriteLine($"Error: invalid command-line option '{currentArg}'.");
+            }
+        }
+
+        public void ShowHelp()
+        {
+            // thanks dotnet-cli for the idea of the help contents.
+            Console.WriteLine($"Version: {Assembly.GetEntryAssembly().GetName().Version}");
+            Console.WriteLine($"usage: {Assembly.GetEntryAssembly().GetName().Name} <command>");
+            Console.WriteLine();
+            Console.WriteLine("Available commands:");
+            Console.WriteLine();
+            foreach (var cmd in this.cmds)
+            {
+                Console.WriteLine($" {(cmd.CommandGroup.Equals("Global") ? string.Empty : cmd.CommandGroup)} {cmd.CommandSwitch}\t{cmd.CommandDescription}");
+                Console.WriteLine();
+            }
+
+            if (this.HasDocs)
+            {
+                Console.WriteLine($"For more information, visit {this.DocsUrl}");
+            }
+        }
+    }
+}
--- newsmake/newsmake/Program.cs
+++ newsmake/newsmake/Program.cs
@@ -15,34 +15,37 @@ namespace Newsmake
         [MiniDump(Text = "Please send a copy of {0} to https://github.com/Elskom/newsmake/issues by making an issue and attaching the log(s) and mini-dump(s).", DumpType = MinidumpTypes.ValidTypeFlags)]
         internal static int Main(string[] args)
         {
-            MiniDump.DumpFailed += MiniDump_DumpFailed;
-            MiniDump.DumpGenerated += MiniDump_DumpGenerated;
+            // MiniDump.DumpFailed += MiniDump_DumpFailed;
+            MiniDump.DumpMessage += MiniDump_DumpMessage;
             _ = Assembly.GetEntryAssembly().EntryPoint.GetCustomAttributes(false);
-            var commandParsor = new CommandParsor(args);
-            commandParsor.AddCommand(new Command("--version", "Global", "Shows the version of this command-line program.", (string[] commands) => Commands.VersionCommand()));
-            commandParsor.AddCommand(new Command("build", "Global", "builds a changelog or news file from any *.master file in the current or sub directory.", (string[] commands) => Commands.BuildCommand()));
-            commandParsor.AddCommand(new Command("release", "new", "Creats a new pending release folder and imports it in the *.master file in the current or sub directory.", (string[] release) => Commands.NewReleaseCommand(release)));
-            commandParsor.AddCommand(new Command("entry", "new", "Creates a new entry file for the current pending release.", (string[] commands) => Commands.NewEntryCommand()));
-            commandParsor.AddCommand(new Command("release", "finalize", "Finalizes a pending release to a section file.", (string[] commands) => Commands.FinalizeReleaseCommand()));
-            if (commandParsor.Length == 0)
+            var commandParser = new CommandParser(args);
+            commandParser += new Command[]
             {
-                commandParsor.ShowHelp();
+                new Command("--version", "Global", "Shows the version of this command-line program.", (string[] commands) => Commands.VersionCommand()),
+                new Command("build", "Global", "builds a changelog or news file from any *.master file in the current or sub directory.", (string[] commands) => Commands.BuildCommand()),
+                new Command("release", "new", "Creates a new pending release folder and imports it in the *.master file in the current or sub directory.", (string[] release) => Commands.NewReleaseCommand(release)),
+                new Command("entry", "new", "Creates a new entry file for the current pending release.", (string[] commands) => Commands.NewEntryCommand()),
+                new Command("release", "finalize", "Finalizes a pending release to a section file.", (string[] commands) => Commands.FinalizeReleaseCommand()),
+            };
+            if (commandParser.Length == 0)
+            {
+                commandParser.ShowHelp();
             }
             else
             {
-                commandParsor.ProcessCommands();
+                commandParser.ProcessCommands();
             }
 
             return 0;
         }
 
-        private static void MiniDump_DumpGenerated(object sender, MessageEventArgs e)
+        private static void MiniDump_DumpMessage(object sender, MessageEventArgs e)
         {
             Console.WriteLine($"{e.Caption}: {e.Text}");
-            Environment.Exit(1);
+            if (!e.Text.StartsWith("Mini-dumping failed with Code: "))
+            {
+                Environment.Exit(1);
+            }
         }
-
-        private static void MiniDump_DumpFailed(object sender, MessageEventArgs e)
-            => Console.WriteLine($"{e.Caption}: {e.Text}");
     }
 }
